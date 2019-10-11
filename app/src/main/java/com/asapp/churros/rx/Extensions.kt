3245b899a4f85070d16b606bd16afe222ce545e8
Guillermo Alcantara <galcantara@asapp.com>
2019-10-10 17:27:08 -0700

    Add disposeWith

--- app/src/main/java/com/asapp/churros/rx/Extensions.kt
+++ app/src/main/java/com/asapp/churros/rx/Extensions.kt
@@ -3,8 +3,14 @@ package com.asapp.churros.rx
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.disposables.DisposableContainer
 
 fun <T> Completable.andThenRun(block: () -> Single<T>): Single<T> = andThen(Single.defer(block))
+
+fun Disposable.disposeWith(disposableContainer: DisposableContainer) {
+    disposableContainer.add(this)
+}
 
 fun <T> Single<T>.runIf(condition: Boolean, block: Single<T>.() -> Single<T>): Single<T> {
     return if (condition) {
